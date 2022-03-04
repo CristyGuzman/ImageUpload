f17c27633a424a6ef94d7e2102012da23cccaf4c
CristyGuzman <csolis@student.ethz.ch>
2022-03-04 20:03:10 +0100

    create app

---
+++ backend/app.py
@@ -0,0 +1,30 @@
+import uvicorn
+from typing import List
+from fastapi import FastAPI, UploadFile, Response
+from pydantic_models.photo_model import PhotoModel
+
+app = FastAPI()
+
+
+@app.get("/status")
+def check_status():
+    return "Hello World"
+
+
+# @app.get("/photos", response_model=List[PhotoModel])
+# def get_photos():
+
+
+@app.post("/add_photo")
+def add_photo(file: UploadFile):
+
+    uploaded_file_url = (
+        f"https://github.com/CristyGuzman/ImageUpload/blob/main/images/{file.filename}"
+    )
+
+    print(f"File uploaded is {file.filename} with content {file.content_type}")
+    return {"file": file.filename, "content": file.content_type}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
